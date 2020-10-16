36af2ab7c8edc4caabc9e7330e02d322b8829334
Vitaliy <theyamshikov@yandex.ru>
2020-10-16 18:06:05 +0600

    upd

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -5,15 +5,14 @@ use ZnLib\Db\Capsule\Manager;
 use ZnExample\ComposerPackage\Symfony4\Console\Commands\BookCommand;
 use ZnExample\ComposerPackage\Domain\Services\BookService;
 use ZnExample\ComposerPackage\Domain\Repositories\Eloquent\BookRepository;
+use Illuminate\Container\Container;
+use ZnLib\Console\Symfony4\Helpers\CommandHelper;
 
 /**
  * @var Application $application
+ * @var Container $container
  */
 
-$eloquentConfigFile = $_ENV['ELOQUENT_CONFIG_FILE'];
-$capsule = new Manager(null, $eloquentConfigFile);
-
-$bookRepository = BookRepository($capsule);
-$bookService = BookService($bookRepository);
-$bookCommand = new BookCommand(BookCommand::getDefaultName(), $bookService);
-$application->add($bookCommand);
+CommandHelper::registerFromNamespaceList([
+    'ZnExample\ComposerPackage\Symfony4\Console\Commands',
+], $container);
