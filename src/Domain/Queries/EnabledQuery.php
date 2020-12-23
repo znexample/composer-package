28d3835dafa4aa98b3ea1bfa9df39c18acb5144c
Vitaliy <theyamshikov@yandex.ru>
2020-12-23 20:44:52 +0600

    upd

--- src/Domain/Queries/EnabledQuery.php
+++ src/Domain/Queries/EnabledQuery.php
@@ -10,7 +10,7 @@ class EnabledQuery extends Query
 
     public function __construct()
     {
-        $this->where('status', StatusEnum::ENABLE);
+        $this->where('status', StatusEnum::ENABLED);
     }
 
 }
