f6b8373ebfa05beb71a1ebb1dc9e1fb06517bdb8
Vitaliy <theyamshikov@yandex.ru>
2021-02-11 10:11:03 +0600

    upd

--- src/Domain/Services/BookService.php
+++ src/Domain/Services/BookService.php
@@ -11,7 +11,7 @@ class BookService extends BaseCrudService implements BookServiceInterface
 
     public function __construct(BookRepositoryInterface $repository)
     {
-        $this->repository = $repository;
+        $this->setRepository($repository);
     }
 
 }
